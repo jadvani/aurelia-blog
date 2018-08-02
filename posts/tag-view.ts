eefde5dcf4ff26c69da7f286d448697d21ad6b05
Javier Advani Aguilar <javieradvaniaguilar@Mac-mini-de-mini-4.local>
2018-08-02 12:38:36 +0200

    fixed tag links (pendant to fix archive links); added header-image

--- posts/tag-view.ts
+++ posts/tag-view.ts
@@ -1,7 +1,24 @@
-export class TagView {    
+import { PLATFORM } from 'aurelia-pal';
+import { PostService } from '../common/services/post-service';
+import { inject } from 'aurelia-framework';
+
+@inject(PostService)
+export class TagView {
   message: string;
-  
-  constructor() {
+  tag: any;
+  posts: any;
+
+
+  constructor(private postService: PostService) {
     this.message = 'Tag world';
   }
+  activate(params) {
+    this.tag = params.tag;
+    this.postService.postsByTag(this.tag).then((data: any) => {
+      this.posts = data.posts;
+    }).catch(error => {
+      message: error.message
+    });
+  }
 }
+
